7bb274f6dfb94f0fb87ddd76b1a19fde1c20c020
Navdeep  Singh <navdeep.singh@sonartechnologies.com>
2017-06-06 15:34:43 +1000

    Project structure Redefined

--- ChatterBox/ChatterBox/Controllers/ViewController.swift
+++ ChatterBox/ChatterBox/Controllers/ViewController.swift
@@ -12,14 +12,7 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        var array = [1,2,3,4,5]
-        
-        array.map {
-            $0+1
-        }
-        
-        
+           
 
     }
 }
