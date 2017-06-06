fa93958238c73a11781be9f358c10b9d09ef7bdf
Navdeep  Singh <navdeep.singh@sonartechnologies.com>
2017-06-06 18:29:03 +1000

    Creating the view for LoginVC
    Added center view for loginVC

--- ChatterBox/ChatterBox/Controllers/LoginController.swift
+++ ChatterBox/ChatterBox/Controllers/LoginController.swift
@@ -9,13 +9,32 @@
 import UIKit
 
 class LoginController: UIViewController {
+    
+    // Creating view components 
+    
+    let userInputGatherView : UIView! = {
+    let viewToGatherinputs = UIView()
+    viewToGatherinputs.backgroundColor = UIColor.white
+    viewToGatherinputs.translatesAutoresizingMaskIntoConstraints = false
+    return viewToGatherinputs
+    }()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = UIColor(red: 61, green: 91, blue: 151, alpha: 1)
+        view.backgroundColor = UIColor(red: 61, green: 91, blue: 151)
+        setupViewToGatherUserCredentials()
     }
 
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+    
+    func setupViewToGatherUserCredentials(){
+      view.addSubview(userInputGatherView)
+      userInputGatherView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+      userInputGatherView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+      userInputGatherView.widthAnchor.constraint(equalTo: view.widthAnchor, constant: -24).isActive = true
+      userInputGatherView.heightAnchor.constraint(equalToConstant: 150).isActive = true
     }
 }
