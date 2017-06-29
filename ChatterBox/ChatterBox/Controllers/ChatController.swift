67d39877bcf92d2e33a222e003e4b34632c6b6e0
Navdeep  Singh <navdeep.singh@sonartechnologies.com>
2017-06-29 13:28:21 +1000

    code clean

---
+++ ChatterBox/ChatterBox/Controllers/ChatController.swift
@@ -0,0 +1,17 @@
+//
+//  ChatController.swift
+//  ChatterBox
+//
+//  Created by Navdeep  Singh on 6/22/17.
+//  Copyright © 2017 Navdeep. All rights reserved.
+//
+
+import UIKit
+
+
+class ChatController : UITableViewController {
+   override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
